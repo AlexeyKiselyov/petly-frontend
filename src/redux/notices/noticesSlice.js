c217537af38181e95a4fd474c604241eb8ce7c8a
AlexeyKiselyov <aleshakiselev123@gmail.com>
2023-02-15 23:43:53 +0200

    remade notices slice on builder

--- src/redux/notices/noticesSlice.js
+++ src/redux/notices/noticesSlice.js
@@ -14,68 +14,55 @@ const initialState = {
   notifyNotices: null,
 };
 
+const handlePending = state => {
+  state.isLoading = true;
+  state.error = null;
+  state.notifyNotices = null;
+};
+
+const handleRejected = (state, { payload }) => {
+  state.error = payload;
+  state.isLoading = false;
+};
+
 const noticesSlice = createSlice({
   name: 'notices',
   initialState,
-  extraReducers: {
-    // отримання оголошень по категоріям
-    [fetchNotices.pending]: state => {
-      state.isLoading = true;
-      state.error = null;
-      state.notifyNotices = null;
-    },
-    [fetchNotices.fulfilled]: (state, { payload }) => {
-      state.notices = payload.notices;
-      state.isLoading = false;
-    },
-    [fetchNotices.rejected]: (state, { payload }) => {
-      state.error = payload;
-      state.isLoading = false;
-    },
-    // отримання одного оголошення
-    [fetchOneNotice.pending]: state => {
-      state.isLoading = true;
-      state.error = null;
-      state.notifyNotices = null;
-    },
-    [fetchOneNotice.fulfilled]: (state, { payload }) => {
-      state.oneNotice = payload;
-      state.isLoading = false;
-    },
-    [fetchOneNotice.rejected]: (state, { payload }) => {
-      state.error = payload;
-      state.isLoading = false;
-    },
-    // додавання оголошень відповідно до обраної категорії
-    [addNotice.pending]: state => {
-      state.isLoading = true;
-      state.error = null;
-      state.notifyNotices = null;
-    },
-    [addNotice.fulfilled]: (state, { payload }) => {
-      state.notices.push(payload);
-      state.notifyNotices = 'Notice was successfully added!';
-      state.isLoading = false;
-    },
-    [addNotice.rejected]: (state, { payload }) => {
-      state.error = payload;
-      state.isLoading = false;
-    },
-    //  видалення оголошення авторизованого користувача створеного цим же користувачем
-    [deleteNotice.pending]: state => {
-      state.isLoading = true;
-      state.error = null;
-      state.notifyNotices = null;
-    },
-    [deleteNotice.fulfilled]: (state, { payload }) => {
-      state.notices = state.notices.filter(({ _id }) => _id !== payload);
-      state.notifyNotices = 'Notice was successfully deleted!';
-      state.isLoading = false;
-    },
-    [deleteNotice.rejected]: (state, { payload }) => {
-      state.error = payload;
-      state.isLoading = false;
-    },
+  extraReducers: builder => {
+    builder
+      // отримання оголошень по категоріям
+      .addCase(fetchNotices.pending, handlePending)
+      .addCase(fetchNotices.fulfilled, (state, { payload }) => {
+        state.notices = payload.notices;
+        state.isLoading = false;
+      })
+      .addCase(fetchNotices.rejected, handleRejected)
+
+      // отримання одного оголошення
+      .addCase(fetchOneNotice.pending, handlePending)
+      .addCase(fetchOneNotice.fulfilled, (state, { payload }) => {
+        state.oneNotice = payload;
+        state.isLoading = false;
+      })
+      .addCase(fetchOneNotice.rejected, handleRejected)
+
+      // додавання оголошень відповідно до обраної категорії
+      .addCase(addNotice.pending, handlePending)
+      .addCase(addNotice.fulfilled, (state, { payload }) => {
+        state.notices.push(payload);
+        state.notifyNotices = 'Notice was successfully added!';
+        state.isLoading = false;
+      })
+      .addCase(addNotice.rejected, handleRejected)
+
+      // видалення оголошення авторизованого користувача створеного цим же користувачем
+      .addCase(deleteNotice.pending, handlePending)
+      .addCase(deleteNotice.fulfilled, (state, { payload }) => {
+        state.notices = state.notices.filter(({ _id }) => _id !== payload);
+        state.notifyNotices = 'Notice was successfully deleted!';
+        state.isLoading = false;
+      })
+      .addCase(deleteNotice.rejected, handleRejected);
   },
   reducers: {
     clearNotices(state, { payload }) {
