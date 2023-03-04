d694425036cc070825b925ede9b9d053704969f8
AlexeyKiselyov <aleshakiselev123@gmail.com>
2023-03-04 23:47:52 +0200

    change avatar img object-fit

--- src/components/UserData/UserData.styled.js
+++ src/components/UserData/UserData.styled.js
@@ -66,6 +66,7 @@ export const Img = styled.img`
   border-radius: 50%;
   box-shadow: 0px 4px 14px rgba(0, 0, 0, 0.11);
   background-color: ${p => p.theme.colors.background};
+  object-fit: cover;
 `;
 export const ImageBox = styled.div`
   display: flex;
